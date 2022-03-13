4230acc20ae5b180eaca0a3ffbaac92fc3c3dcc6
ender <ender@192.168.1.17>
2022-03-13 15:09:20 -0300

    criando uma classe abstrata Pessoa

--- src/modelo/Pessoa.java
+++ src/modelo/Pessoa.java
@@ -1,13 +1,19 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package modelo;
 
-/**
- *
- * @author ender
- */
-public interface Pessoa {
+public abstract class Pessoa {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
     
 }
