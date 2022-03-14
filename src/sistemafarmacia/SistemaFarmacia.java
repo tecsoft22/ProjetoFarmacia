e5202a93277a16c58be687419351ccc984430368
Enderson Lynhares <enderson.linhares20@gmail.com>
2022-03-13 21:42:51 -0300

    adicionando imagens e alguns pacotes adicionais e também algumas classes e telas

--- src/sistemafarmacia/SistemaFarmacia.java
+++ src/sistemafarmacia/SistemaFarmacia.java
@@ -1,7 +1,11 @@
 package sistemafarmacia;
 
+import visao.Principal;
+
 public class SistemaFarmacia {
     public static void main(String[] args) {
-        
+        Principal tela;
+        tela = new Principal(); 
+        tela.setVisible(true);
     } 
 }
