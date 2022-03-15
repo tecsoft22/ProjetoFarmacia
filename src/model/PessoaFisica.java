0822bf2db32d58bdcf50038018d00f4abbfd0f77
admin <admin@DESKTOP-22IM4Q5>
2022-03-15 15:15:03 -0300

    c vdffffghgh

--- src/model/PessoaFisica.java
+++ src/model/PessoaFisica.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
-/**
- *
- * @author admin
- */
-public class PessoaFisica {
+public class PessoaFisica extends Pessoa{
+    
+    private String cpf;
+    private String rg;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+    
+    
     
 }
